111bd881acac7db033807e6fa12ea84207de1490
Szabolcs Dombi <cprogrammer1994@gmail.com>
2017-06-20 21:18:44 +0200

    compact setup args

--- setup.py
+++ setup.py
@@ -3,21 +3,15 @@ from setuptools import setup
 package = 'ModernGL.ext.textools'
 version = '0.1.0'
 
-args = {
-	'name': package,
-	'version': version,
-
-	'author': 'Szabolcs Dombi',
-	'author_email': 'cprogrammer1994@gmail.com',
-
-	'description': 'ModernGL extension for handling textures easily',
-	'url': 'https://github.com/cprogrammer1994/ModernGL.ext.textools',
-	'license': 'MIT',
-
-	'packages': [package],
-
-	'install_requires': ['ModernGL', 'Pillow', 'numpy'],
-	'platforms': ['any'],
-}
-
-setup(**args)
+setup(
+	name=package,
+	version=version,
+	author='Szabolcs Dombi',
+	author_email='cprogrammer1994@gmail.com',
+	description='ModernGL extension for handling textures easily',
+	url='https://github.com/cprogrammer1994/ModernGL.ext.textools',
+	license='MIT',
+	packages=[package],
+	install_requires=['ModernGL', 'Pillow', 'numpy'],
+	platforms=['any'],
+)
