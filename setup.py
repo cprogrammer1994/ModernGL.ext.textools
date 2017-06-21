e034e79f43442eec463da1b2d22eb4ddc6e1a237
Szabolcs Dombi <cprogrammer1994@gmail.com>
2017-06-21 17:07:38 +0200

    rollback to distutils (eggs from setuptools are not working)

--- setup.py
+++ setup.py
@@ -1,17 +1,14 @@
-from setuptools import setup
-
-package = 'ModernGL.ext.textools'
-version = '0.1.0'
+from distutils.core import setup
 
 setup(
-	name=package,
-	version=version,
+	name='ModernGL.ext.textools',
+	version='0.1.0',
 	author='Szabolcs Dombi',
 	author_email='cprogrammer1994@gmail.com',
 	description='ModernGL extension for handling textures easily',
 	url='https://github.com/cprogrammer1994/ModernGL.ext.textools',
 	license='MIT',
-	packages=[package],
+	packages=['ModernGL.ext.textools'],
 	install_requires=['ModernGL', 'Pillow', 'numpy'],
 	platforms=['any'],
 )
